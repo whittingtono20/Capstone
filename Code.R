5b3fe3e3f488802b357202decffae3917afd21bc
Mikki Whittington <whittingtono20@mail.wlu.edu>
2020-02-24 11:14:19 -0500

    Changing school names to school codes through schools starting with C

--- Code.R
+++ Code.R
@@ -2,8 +2,16 @@ library(tidyverse)
 Final_Data <- Final_Data_Feb_24
 
 #Change gender codes to F, M, NB
-Final_Data$Q1[Final_Data$Q1 == "Female"] <- "F" 
-Final_Data$Q1[Final_Data$Q1 == "Male"] <- "M"
+Final_Data$Q1[Final_Data$Q1 == "Female"]    <- "F" 
+Final_Data$Q1[Final_Data$Q1 == "Male"]      <- "M"
 Final_Data$Q1[Final_Data$Q1 == "Nonbinary"] <- "NB"
 
 #Change school name to 2-3 letter code
+Final_Data$Q6[Final_Data$Q6 == "Amherst College"]               <- "AC"
+Final_Data$Q6[Final_Data$Q6 == "Andrews University"]            <- "AU"
+Final_Data$Q6[Final_Data$Q6 == "app state"]                     <- "ASU"
+Final_Data$Q6[Final_Data$Q6 == "Appalachian State"]             <- "ASU"
+Final_Data$Q6[Final_Data$Q6 == "Appalachian State University"]  <- "ASU"
+Final_Data$Q6[Final_Data$Q6 == "Kalamazoo College"]             <- "KC"
+Final_Data$Q6[Final_Data$Q6 == "Washington and Lee University"] <- "WLU"
+
